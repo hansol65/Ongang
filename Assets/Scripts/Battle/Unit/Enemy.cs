55eabb8f5d8f842282ac5878d6b5a86cfa712fab
Emithen <86219540+Emithen@users.noreply.github.com>
2025-01-14 15:54:49 +0900

    setting: directory, script deactive
    스크립트 일부 이동
    Player, Enemy 스크립트 주석처리

---
+++ Assets/Scripts/Battle/Unit/Enemy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Enemy : Unit
+{
+    /*
+    public override void Initialize(string name, int health, int attack)
+    {
+        base.Initialize(name, health, attack);
+        Debug.Log($"Player {name} initialized with {health} HP, {attack} Attack.");
+    }
+    **/
+
+    // Add Enemy-specific methods here (e.g., abilities, inventory management)
+}
